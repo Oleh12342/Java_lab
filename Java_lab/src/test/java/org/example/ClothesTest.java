20d0a0966c8f6ffad18334aaab09180a5e9feb30
Oleh12342 <148885488+Oleh12342@users.noreply.github.com>
2026-03-22 18:04:22 +0200

    Add unit tests for validation logic.

---
+++ Java_lab/src/test/java/org/example/ClothesTest.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class ClothesTest {
+
+    @Test
+    void shouldThrowExceptionWhenInvalidSize() {
+        Clothes clothes = new Clothes("T-shirt", 42, 500.0, "Cotton");
+        assertThrows(IllegalArgumentException.class, () -> clothes.setSize(-1));
+    }
+
+    @Test
+    void shouldThrowExceptionWhenConstructorDataInvalid() {
+        assertThrows(IllegalArgumentException.class, () ->
+                new Clothes("", 0, -10.0, null));
+    }
+}
