2874473926e21ebdaef4c8bf75bf4f02b49ab9c7
Oleh12342 <148885488+Oleh12342@users.noreply.github.com>
2026-03-26 20:27:27 +0200

    Creating a derived class Shirts.

---
+++ Java_lab/src/main/java/org/example/Shirts.java
@@ -0,0 +1,21 @@
+package org.example;
+
+public class Shirts extends Clothes {
+    private String sleeveType; // Наприклад: "Короткий" або "Довгий"
+
+    public Shirts(String name, int size, double price, String material, String sleeveType) {
+        super(name, size, price, material);
+        setSleeveType(sleeveType);
+    }
+
+    public void setSleeveType(String sleeveType) {
+        if (sleeveType == null || sleeveType.trim().isEmpty())
+            throw new IllegalArgumentException("Тип рукава не вказано");
+        this.sleeveType = sleeveType;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + String.format(", Рукав: %s [Тип: Сорочка]", sleeveType);
+    }
+}
