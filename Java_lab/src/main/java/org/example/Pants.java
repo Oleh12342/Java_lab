8b16c82e034c1d2b1e5e4e8ad203046c2908be1e
Oleh12342 <148885488+Oleh12342@users.noreply.github.com>
2026-03-26 20:28:00 +0200

    Creating a derived class Pants.

---
+++ Java_lab/src/main/java/org/example/Pants.java
@@ -0,0 +1,20 @@
+package org.example;
+
+public class Pants extends Clothes {
+    private int length;
+
+    public Pants(String name, int size, double price, String material, int length) {
+        super(name, size, price, material);
+        setLength(length);
+    }
+
+    public void setLength(int length) {
+        if (length <= 0) throw new IllegalArgumentException("Довжина має бути більше 0");
+        this.length = length;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + String.format(", Довжина: %d см [Тип: Штани]", length);
+    }
+}
