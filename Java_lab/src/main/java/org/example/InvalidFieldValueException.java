f5a12e2c52a11a842fcd4f9447ccf2b75844b624
Oleh12342 <148885488+Oleh12342@users.noreply.github.com>
2026-05-02 14:51:49 +0300

    Creating class 2 exceptions.

---
+++ Java_lab/src/main/java/org/example/InvalidFieldValueException.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class InvalidFieldValueException extends RuntimeException {
+    public InvalidFieldValueException(String message) {
+        super(message);
+    }
+}
